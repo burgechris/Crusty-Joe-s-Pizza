5e55e48cb925977622ee8f72ee9f82d379b9b7bf
Chris Burge <topher.burge@gmail.com>
2019-08-30 10:06:56 -0700

    add console.log for testing purposes.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,22 @@
 //Business Logic
 
 //Order/Shopping Cart Object
-function Cart(name){
-  this.name = name,
-  this.items = [],
-  this.total = 0
-}
-
-//User adds the pizza to the Cart
+// function Cart(name){
+//   this.name = name,
+//   this.pizzas = [],
+//   this.total = 0
+// }
+//
+// //User adds the pizza to the Cart
+// Cart.prototype.total = function(){
+//   var total = 0;
+//
+// }
 
 //Pizza Object
 //User chooses what size pizza they would like
-function Pizza(size, toppings){
+function Pizza(name, size, toppings){
+  this.name = name,
   this.size = size,
   this.sauce = sauce,
   this.toppings = [],
@@ -24,30 +29,27 @@ Pizza.prototype.toppings = function(){
   this.toppings = addTopping.push(this.toppings);
 }
 
-Pizza.prototype.calc = function(){
+Pizza.prototype.calcToppers = function(){
   this.subtotal = this.toppings.length + 5;
 }
-
-
-
 
 
 
 //User-Interface Logic
 
 //Creates a new Cart
-var newCart = new Cart();
+// var newCart = new Cart();
 
 $(document).ready(function() {
   $("button#addToCart").click(function(event){
     event.preventDefault();
     //User enters name
-    var customerName = $("#custName").val();
+    var customerName = $("input#custName").val();
     var size = $("input:radio[name=size]:checked").val();
-    var saucy = $("input:radio[name=sauce]:checked").val();
+    var sauce = $("input:radio[name=sauce]:checked").val();
     var toppers = $("input:checkbox[name=topping]:checked").val();
+    console.log(customerName, size, sauce, toppers);
+    var newPizza = new Pizza(customerName, size, sauce, toppers);
 
-    var newPizza = new Pizza(size, saucy, toppers);
-    
   });
 });
