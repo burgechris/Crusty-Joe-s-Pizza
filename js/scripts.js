55cfdcbb9a9626cc418f86019bc87a323b51f2a0
Chris Burge <topher.burge@gmail.com>
2019-08-30 08:54:58 -0700

    add a cart object and a pizza object to the script.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@
 //Business Logic
 
 //Order/Shopping Cart Object
+function Cart(name){
+  this.name = name,
+  this.items = [],
+  this.total = 0
+}
+
+//User adds the pizza to the Cart
+
+//Pizza Object
+function Pizza(toppings){
+  this.toppings = toppings,
+  this.subtotal = 0
+}
 //User chooses what size pizza they would like
 //User chooses what toppings they would like
-//User adds the pizza to the Cart
 
 
 
@@ -15,6 +27,8 @@
 //User enters name
 
 $(document).ready(function() {
+  $("button#addToCart").click(function(event){
+    event.preventDefault();
 
-
+  });
 });
